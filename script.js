6c3294564d2976c87bf2bbb50c8c8f5a07ef2e88
D4rkLorD <135694320+D4rkMath@users.noreply.github.com>
2025-03-31 10:16:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,21 +12,4 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('.toggle-content').forEach(content => {
         content.style.display = 'none';
     });
-    // Cargar el chatbot de Voiceflow
-    (function(d, t) {
-        var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-        v.onload = function() {
-            window.voiceflow.chat.load({
-                verify: { projectID: '67eaade1770ceb5319c8a89f' },
-                url: 'https://general-runtime.voiceflow.com',
-                versionID: 'production',
-                voice: {
-                    url: "https://runtime-api.voiceflow.com"
-                }
-            });
-        }
-        v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; 
-        v.type = "text/javascript"; 
-        s.parentNode.insertBefore(v, s);
-    })(document, 'script');
 });
